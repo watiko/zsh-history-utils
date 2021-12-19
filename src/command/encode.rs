a9c1fd1f4c73796d2b64444dde8be613f980e283
watiko <service@mail.watiko.net>
2021-12-19 21:47:55 +0900

    feat: encode command

--- src/command/encode.rs
+++ src/command/encode.rs
@@ -1,8 +1,16 @@
 use eyre::Result;
+use std::io::{self, Write};
 use std::path::PathBuf;
 
+use crate::zsh::history::HistoryEntry;
+
 pub fn run(path: &PathBuf) -> Result<()> {
-    println!("{}", path.to_str().unwrap());
+    let file = std::fs::File::open(path)?;
+
+    let iter = serde_json::Deserializer::from_reader(file).into_iter::<HistoryEntry>();
+    for entry in iter {
+        io::stdout().write_all(&entry?.to_bytes())?;
+    }
 
     Ok(())
 }
